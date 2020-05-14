95c9aa2b590a6e6156a0507ddced56e69d07ac7f
Eric Kelley <hello@ericmkelley.com>
2020-05-13 23:44:03 -0400

    placeholder page

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-## Beamed App
+# prelaunch
 
-Explore the source code of Beamed for each distribution by looking at each respective branch.
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Beamed logo" src="./assets/Logo-Full--500px.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {}
+};
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+  overflow: hidden;
+}
+#app {
+  background: rgb(2, 4, 20);
+  background: linear-gradient(
+    0deg,
+    rgba(2, 4, 20, 1) 0%,
+    rgba(3, 7, 34, 1) 100%
+  );
+  height: 100vh;
+  margin: 0;
+  padding-top: 25%;
+  text-align: center;
+}
+</style>

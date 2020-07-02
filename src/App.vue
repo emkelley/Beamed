4e43a272802a51ceef559b0fb8a26dfdc94625da
Eric Kelley <hello@ericmkelley.com>
2020-07-01 23:24:09 -0400

    fixed git fuckery

--- src/App.vue
+++ src/App.vue
@@ -6,30 +6,12 @@
 
 <script>
 export default {
-  name: "App",
+  name: 'App',
   components: {}
-};
+}
 </script>
 
 <style lang="scss">
-<<<<<<< Updated upstream
-body {
-  margin: 0;
-  overflow: hidden;
-}
-#app {
-  background: rgb(2, 10, 37);
-  background: radial-gradient(
-    circle,
-    rgba(2, 10, 37, 1) 0%,
-    rgba(1, 7, 28, 1) 100%
-  );
-  height: 100vh;
-  margin: 0;
-  padding-top: 25%;
-  text-align: center;
-}
-=======
 @import '~bulma/sass/utilities/_all';
 #app {
   background: $navy;
@@ -100,5 +82,4 @@ $link-focus-border: $primary;
 
 @import '~bulma';
 @import '~buefy/src/scss/buefy';
->>>>>>> Stashed changes
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-<<<<<<< Updated upstream
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-=======
 import router from './router'
 import store from './store'
 import Buefy from 'buefy'
@@ -17,8 +10,7 @@ Vue.component('vue-headful', vueHeadful)
 Vue.config.productionTip = false
 
 new Vue({
-	router,
-	store,
-	render: h => h(App)
->>>>>>> Stashed changes
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
